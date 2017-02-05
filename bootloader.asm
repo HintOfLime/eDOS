5cb33a7f68d4b92114f39ec60f4a49d6b74a47e3
Edward Atkinson <edwardatki@gmail.com>
2017-02-05 20:46:00 +0000

    Got the bootloader to work correctly

--- bootloader.asm
+++ bootloader.asm
@@ -3,11 +3,11 @@
 ;+---------------------------------+
 
 [bits 16]
-[org 0]
+[org 0x0000]
 
 start:
-	jmp 0x07C0:loader	; Jump over OEM block
-	nop			; This is supposedly good practice?
+	jmp 0x07C0:loader							; Jump over OEM block
+	nop											; This is supposedly good practice?
  
 ;*************************************************;
 ;	OEM Parameter block / BIOS Parameter Block    ;
@@ -37,118 +37,38 @@ bsFileSystem: 	        DB "FAT12   "
 ;*************************************************;
 
 loader:
-	; Set Data Segments
+	; Setup Segments and Stack
+	cli
 	mov ax, 0x07C0
 	mov	ds, ax
 	mov	es, ax
+	mov ax, 0x0000
+	mov ss, ax
+	mov sp, 0xFFFF
+	sti
 	
 	mov [DISK_VARS.DISK], dl					; Set disk
 	
-	; mov ax, 0
-	; mov cx, 0x0100
-	; test_loop:
-	;	 call Debug
-	;	 inc ax
-	;	 loop test_loop
-	;	
-	; cli
-	; hlt
+	mov ax, Disk.Get_File
+	mov [0x0001], ax
 	
-	;mov si, DISK_VARS.FILENAME
-	;call Print
+	mov si, [DISK_VARS.FILENAME]
+	mov ax, 0x0000								; Offset:	0x0000
+	mov bx, 0x0A00								; Segment:	0x0A00
+	call Disk.Get_File
 	
-	call Disk.Load_RD
-	
-	call Disk.Search_For_File					; Find Second Stage
-	
-	call Disk.Load_FAT							; Load FAT
-	
-	call Disk.Cluster_To_CHS					; Convert Second Stage Cluster to CHS				 --- Returns correct value but hangs ---
-	
-	mov ax, [bpbSectorsPerCluster]
-	mov [DISK_VARS.SECTORS_TO_READ], ax
-	
-	mov ax, 0x0000
-	mov [DISK_VARS.MEM_OFFSET], ax
-	mov ax, 0x0A00
-	mov [DISK_VARS.MEM_SEGMENT], ax
-	
-	call Disk.ReadSectors						; Read Second Stage Cluster
-	
-	mov ax, [0xA000]
-	call Debug
-	mov ax, [0xA001]
-	call Debug
-	
-	cli
-	hlt
-	
-	;jmp 0x0A00:0x0000							; Jump to Second Stage
+	jmp 0x0A00:0x0000							; Jump to Second Stage
 
 ; ----- Routines -----
-; Prints a string
-; SI = Start address of string
-;Print:
-	;push ax
-	;.Loop:
-	;	lodsb
-	;	or al, al
-	;	jz .Exit
-	;	mov	ah, 0x0e
-	;	int	0x10
-	;	jmp .Loop
-	;.Exit:
-	;	pop ax
-	;	ret
-
-; Prints AX as hex
-; AX = Value to print
-Debug:
-	push ax
-	push cx
-	mov cx, 4
-	
-	;mov si, SPACE
-	;call Print
-	
-	.loop:
-		rol ax, 4
-		call .LowALOut
-	
-	loop .loop
-	
-	pop cx
-	pop ax
-	ret
-	
-	.LowALOut:
-		push ax
-		and ax, 0x000F
-		cmp al, 0x9
-		jg .abcdef
-		add al, 0x30
-		jmp .skip
-		.abcdef:
-		add al, 0x37
-		.skip:
-		mov ah, 0x0e
-		int 0x10
-		pop ax
-		ret
-	
-
 Disk:
 	.Reset:
-		pusha
 		mov ah, 0
 		mov dl, [DISK_VARS.DISK]
 		int 0x13
 		jc .Error
-		popa
 		ret
 
 	.ReadSectors:
-		pusha
 		mov ah, 0x02 							; BIOS read sector function
 		mov al, [DISK_VARS.SECTORS_TO_READ]		; Select number of segments
 		push ax
@@ -166,14 +86,41 @@ Disk:
 		pop dx
 		cmp dl, al 								; If AL ( sectors read ) != DL ( sectors expected )
 		jne .Error 								; Jump to error
-		popa
 		ret
-
+	
+	.Load_RD:
+		push ax
+		
+		; Compute size of Root Directory
+		mov ax, 32	        						; ( 32 byte directory entry
+		mul WORD [bpbRootEntries]  					;   * Number of root entrys )
+		mov dx, 0
+		div WORD [bpbBytesPerSector] 				; / Get sectors used by root directory
+		mov [DISK_VARS.SECTORS_TO_READ], al			; Set as parameter
+		mov [DISK_VARS.RD_START], ax
+		
+		; Compute location of Root Directory
+		mov al, [bpbNumberOfFATs]  					; Get number of FATs (Usually 2)
+		mul WORD [bpbSectorsPerFAT]  				; Number of FATs * sectors per FAT
+		add ax, WORD [bpbReservedSectors] 			; Add reserved sectors
+		mov [DISK_VARS.LBA], ax						; Set as parameter
+		mov [DISK_VARS.RD_SIZE], ax
+		call Disk.LBA_To_CHS						; Convert to CHS for disk read
+		
+		mov ax, 0x0000
+		mov [DISK_VARS.MEM_OFFSET], ax
+		mov ax, 0x07E0
+		mov [DISK_VARS.MEM_SEGMENT], ax
+		
+		call Disk.Reset								; Reset disk
+		call Disk.ReadSectors						; Load root directory
+	
+		pop ax
+		ret
+	
 	.Search_For_File:
-		pusha
 		mov cx, [bpbRootEntries]        		; Get the number of entries. If we reach 0, file doesnt exist
 		mov di, 0x0200        					; Root directory was loaded here
-		mov si, [DISK_VARS.FILENAME]
 		.Loop:
 			push cx
 			mov cx, 11
@@ -190,40 +137,9 @@ Disk:
 		.Exit:
 			mov ax, WORD [di + 26]				; Get First Logical Cluster
 			mov WORD [DISK_VARS.CLUSTER], ax
-			popa
 			ret
 	
-	.Load_RD:
-		push ax
-		
-		; Compute size of Root Directory
-		mov ax, 32	        						; ( 32 byte directory entry
-		mul WORD [bpbRootEntries]  					;   * Number of root entrys )
-		div WORD [bpbBytesPerSector] 				; / Get sectors used by root directory
-		mov [DISK_VARS.SECTORS_TO_READ], al			; Set as parameter
-		mov [RD_START], ax
-		
-		; Compute location of Root Directory
-		mov al, [bpbNumberOfFATs]  					; Get number of FATs (Usually 2)
-		mul WORD [bpbSectorsPerFAT]  				; Number of FATs * sectors per FAT
-		add ax, WORD [bpbReservedSectors] 			; Add reserved sectors
-		mov [DISK_VARS.LBA], al						; Set as parameter
-		mov [RD_SIZE], ax
-		call Disk.LBA_To_CHS						; Convert to CHS for disk read
-		
-		mov ax, 0x0000
-		mov [DISK_VARS.MEM_OFFSET], ax
-		mov ax, 0x07E0
-		mov [DISK_VARS.MEM_SEGMENT], ax
-		
-		call Disk.Reset								; Reset disk
-		call Disk.ReadSectors						; Load root directory
-	
-		pop ax
-		ret
-	
 	.Load_FAT:
-		pusha
 		mov ax, 0
 		mov al, [bpbNumberOfFATs]				; Number of FATs
 		mul WORD [bpbSectorsPerFAT]				; Multiply by Sectors Per FAT
@@ -234,31 +150,21 @@ Disk:
 		
 		call Disk.LBA_To_CHS
 		
-		mov ax, 0x0000
-		mov [DISK_VARS.MEM_OFFSET], ax
-		mov ax, 0x07E0
-		mov [DISK_VARS.MEM_SEGMENT], ax
-		
 		call Disk.ReadSectors
-		popa
 		ret
 	
 	 .Cluster_To_LBA:							; LBA = (Cluster - 2 ) * SectorsPerCluster	???
-		pusha
 		mov ax, WORD [DISK_VARS.CLUSTER]
-		add ax, [RD_START]
-		add ax, [RD_SIZE]
+		add ax, [DISK_VARS.RD_START]
+		add ax, [DISK_VARS.RD_SIZE]
 		sub ax, 2                          		; Subtract 2 from cluster number
 		mov cx, 0
 		mov cl, BYTE [bpbSectorsPerCluster]     ; Get sectors per cluster
 		mul cx                                  ; Multiply
 		mov [DISK_VARS.LBA], ax
-		call Debug
-		popa
 		ret
 		
 	.LBA_To_CHS:
-		pusha
 		;Absolute Sector 	= 	(LBA  %  SectorsPerTrack) + 1
 		;Absolute Head   	= 	(LBA  /  SectorsPerTrack) % Heads
 		;Absolute Cylinder 	= 	 LBA  / (SectorsPerTrack  * Heads)
@@ -269,12 +175,13 @@ Disk:
 		inc dx									; + 1
 		mov BYTE [DISK_VARS.SECTOR], dl
 		
-		mov dx, 0																					; --- Hangs here when converting from cluster ---
+		mov dx, 0
 		mov ax, WORD [DISK_VARS.LBA]
 		div WORD [bpbSectorsPerTrack]			; (LBA / SectorsPerTrack)
+		mov dx, 0
 		div WORD [bpbHeadsPerCylinder]			; % Heads
 		mov BYTE [DISK_VARS.HEAD], dl
-
+		
 		mov dx, 0
 		mov ax, WORD [bpbSectorsPerTrack]
 		mul WORD [bpbHeadsPerCylinder]			; (SectorsPerTrack * Heads)
@@ -283,30 +190,88 @@ Disk:
 		div cx
 		mov BYTE [DISK_VARS.CYLINDER], al
 		
-		popa
 		ret
 		
-	.Cluster_To_CHS:
-		call Disk.Cluster_To_LBA				; HANGS HERE!!! (because cluster is 0 so LBA goes negative on SUB AX, 2)
-		call Disk.LBA_To_CHS
+	.Load_File:
+		.start:
+			mov al, [bpbSectorsPerCluster]
+			mov [DISK_VARS.SECTORS_TO_READ], al
+			
+			call Disk.Cluster_To_LBA					; Convert Cluster to LBA
+			call Disk.LBA_To_CHS						; And then to CHS
+			call Disk.ReadSectors
+			
+			mov     ax, WORD [DISK_VARS.CLUSTER]        ; Identify current cluster
+			mov     cx, ax                              ; Copy current cluster
+			mov     dx, ax                              ; Copy current cluster
+			shr     dx, 0x0001                          ; Divide by two
+			add     cx, dx                              ; Sum for (3/2)
+			mov     bx, 0x0200                          ; Location of FAT in memory
+			add     bx, cx                              ; Index into FAT
+			mov     dx, WORD [bx]                       ; Read two bytes from FAT
+			test    ax, 0x0001
+			jnz     .odd
+			jmp .even
+			
+		.odd:
+			shr     dx, 0x0004                          ; Take high twelve bits
+			jmp .get_cluster
+		
+		.even:
+			and     dx, 0x0FFF               			; Take low twelve bits
+			jmp .get_cluster
+		
+		.get_cluster:
+			mov     WORD [DISK_VARS.CLUSTER], dx                  ; store new cluster
+			cmp     dx, 0x0FF0                          ; test for end of file
+			jb      start
+			ret
+		
+	.Get_File:
+		push ax
+		push bx
+		push si
+		
+		mov ax, 0x07C0
+		mov	ds, ax
+		mov	es, ax
+		
+		call Disk.Load_RD							; Load Root Directory
+		pop si
+		call Disk.Search_For_File					; Find Second Stage
+		call Disk.Load_FAT							; Load FAT
+	
+		pop bx
+		mov [DISK_VARS.MEM_SEGMENT], bx
+		pop ax
+		mov [DISK_VARS.MEM_OFFSET], ax
+	
+		call Disk.Load_File
 		ret
 		
 	.Error:
-		;mov si, DISK_VARS.ERROR_MSG
-		;call Print
+		mov ah, 0x0e
+		mov al, "E"
+		int 0x10
 		cli
 		hlt
 		
 ; ----- Variables -----
 DISK_VARS:
-	.CYLINDER:
-		db 0
-	.HEAD:
-		db 0
-	.DISK:
-		db 0
-	.SECTOR:
-		db 0
+	;.CYLINDER:
+		;db 0
+	;.HEAD:
+		;db 0
+	;.DISK:
+		;db 0
+	;.SECTOR:
+		;db 0
+		
+	DISK_VARS.CYLINDER EQU 0x0400			; To save space
+	DISK_VARS.HEAD EQU 0x0401
+	DISK_VARS.DISK EQU 0x0402
+	DISK_VARS.SECTOR EQU 0x0403
+	
 	.SECTORS_TO_READ:
 		db 0
 	.MEM_SEGMENT:
@@ -315,18 +280,23 @@ DISK_VARS:
 		dw 0x0000
 	.FILENAME:
 		db "SECTOR2 SYS", 0
-	.CLUSTER:
+		
+	.CLUSTER:								; That trick doesn't work here for some reason - ???
 		dw 0
 	.LBA:
 		dw 0
-	;.ERROR_MSG:
-		;db "Disk Error", 0
-SPACE:
-	db " ", 0
-RD_START:
-	dw 0
-RD_SIZE:
-	dw 0
+	.RD_START:
+		dw 0
+		
+	;DISK_VARS.CLUSTER EQU 0x0404
+	;DISK_VARS.LBA EQU 0x0406
+	;DISK_VARS.RD_START EQU 0x0408
+	;DISK_VARS.RD_SIZE EQU 0x040A
+	
+	; 0x0000 - 0x01FF	-	Bootloader
+	; 0x0200 - 0x03FF	-	FAT / RD
+	; 0x0400 - 0x05FF	-	VARs
 	
 times 510 - ($-$$) db 0
-dw 0xaa55
+.RD_SIZE:	;1337 H4X0R to save space
+dw 0xAA55
--- sector2.asm
+++ sector2.asm
@@ -3,22 +3,86 @@
 ;+---------------------------------+
 
 [bits 16]
-[org 0x0A00:0x0000]
+[org 0x0000]
+
 
 ; ----- Main -----
 
-;mov ax, 0x0A00
-;mov	ds, ax
-;mov	es, ax
+; Setup Segments and Stack
+cli
+mov ax, 0x0A00
+mov	ds, ax
+mov	es, ax
+mov ax, 0x0000
+mov ss, ax
+mov sp, 0xFFFF
+sti
 
-;mov si, TestString
-;call Print
+mov si, TestString
+call Print
 
-;cli
-;hlt
+call 0x07C0:0x0000							; This returns a disk error (probably because no parameters were set)
 
+cli
+hlt
+
+
+; ----- Routines -----
+; Prints a string
+; SI = Start address of string
+Print:
+	push ax
+	.Loop:
+		lodsb
+		or al, al
+		jz .Exit
+		mov	ah, 0x0e
+		int	0x10
+		jmp .Loop
+	.Exit:
+		pop ax
+		ret
+
+; Prints AX as hex
+; AX = Value to print
+Debug:
+	push ax
+	push cx
+	mov cx, 4
+	
+	.loop:
+		rol ax, 4
+		call .LowALOut
+	
+	loop .loop
+	
+	mov ah, 0x0e
+	mov al, 0x0A
+	int 0x10
+	mov al, 0x0D
+	int 0x10
+	
+	pop cx
+	pop ax
+	ret
+	
+	.LowALOut:
+		push ax
+		and ax, 0x000F
+		cmp al, 0x9
+		jg .abcdef
+		add al, 0x30
+		jmp .skip
+		.abcdef:
+		add al, 0x37
+		.skip:
+		mov ah, 0x0e
+		int 0x10
+		pop ax
+		ret
+		
 ; ----- Data -----
-;TestString:
-;db "Hello World!", 0
-
-db 0xDE, 0xAD
+DEAD:
+	dw 0xDEAD
+TestString:
+	db "Hello World!", 0x0A, 0x0D, 0
